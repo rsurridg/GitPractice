777e87e08bd0a9cc55e54630d4888143746cbd77
Rachel Surridge <rsurridg@iupui.edu>
2020-07-17 09:37:21 -0400

    initial commit

---
+++ src/com/learnstuff/Main.java
@@ -0,0 +1,11 @@
+package com.learnstuff;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // here's a comment
+        var y = 2;
+        var x = 6*y;
+        System.out.println(x);
+    }
+}
